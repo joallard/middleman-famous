a3c057f41e480fe926feaa137b46b535b11e6377
Jonathan Allard <jonathan@allard.io>
2013-09-15 14:37:44 -0400

    Change MD parser to Redcarpet

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 ruby "2.0.0"
 
 gem "middleman", "~> 3.1.0"
-
 gem "zurb-foundation", "~> 4.3.0"
+
+gem "redcarpet" # Better Markdown
--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+set :markdown_engine, :redcarpet
+
 activate :livereload
 
 # Build-specific configuration
